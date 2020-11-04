4f7a10b5296fecc83a9abb86601a03796676ba20
Tommy Apter <tommyapter@gmail.com>
2020-11-03 17:04:48 -0800

    Updated admin to support custom user model

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,27 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+from core import models
 
-# Register your models here.
+
+# import the default Django user admin and we just need to change some of the class variables to support our custom user admin.
+# because there's still a few changes that we need to make to our Django admin class to support our custom user model the Edit Page won't work in its current state. So we're going to make some changes to make sure that it does work.
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),  # None because this is the title for the section. Each one of these brackets is a section
+        (_('Personal Info'), {'fields': ('name',)}),  # make sure you add this comma ('name',) because otherwise it thinks this is just a string and it won't work.
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',)}),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,44 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse  # allow us to generate URLs for our Django admin page.
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):  # setup function is a function that is ran before every test that we run so sometimes there are setup tasks that need to be done before every test in our test case class.
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@londonappdev.com',
+            password='password123'
+        )
+        self.client.force_login(self.admin_user)  # force_login allows you to log a user in with the Django authentication and this really helps make our tests a lot easier to write because it means we don't have to manually log the user in.
+        self.user = get_user_model().objects.create_user(
+            email='test@londonappdev.com',
+            password='password123',
+            name='Test User Full Name',
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on the user page"""     # test that the users are listed in our Django admin. The reason we need to add a test for this is because we need to slightly customize the Django admin to work with our custom user model so as explained previously the default user model expects a username and as such the default Django admin for the user model also expects a username which we don't have user name we just have the email address so we need to make a few small changes to our admin.py file just to make sure it supports our custom user model.
+        # reverse will generate the URL for our list user page. We use this reverse function instead of just typing the URL manually is because if we ever want to change the URL in a future it means we don't have to go through and change it everywhere in our test because it should update automatically based on reverse.
+        url = reverse('admin:core_user_changelist')  # pass to reverse, the app that you're going for : the URL that you want; These URLs are actually defined in the Django admin documentation
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)  # assertContains check that our response here contains a certain item. It checks that the HTTP response was HTTP 200 and that it looks into the actual content of this res because if you were to manually output this res it's just an object so it's intelligent enough to look into the actual output that is rendered and to check for the contents there
+        self.assertContains(res, self.user.email)
+
+    def test_user_page_change(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])  # reverse function will create a URL like /admin/core/user/id of the user. And this is how the args argument works in the reverse function. So basically anything we pass in here will get assigned to the arguments of the URL here at the end so that's how we customize the ID.
+
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+
+#     def test_create_user_page(self):
+#         """Test that the create user page works"""
+#         url = reverse('admin:core_user_add')
+#         res = self.client.get(url)
+#
+#         self.assertEqual(res.status_code, 200)
